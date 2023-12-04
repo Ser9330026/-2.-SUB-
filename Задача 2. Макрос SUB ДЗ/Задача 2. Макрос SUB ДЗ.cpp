e8f3514538c14cca39ef26bc08c2151563b75418
ser9330026 <ser9330026@yandex.ru>
2023-12-04 18:31:18 +0500

    Добавьте файлы проекта.

---
+++ Задача 2. Макрос SUB ДЗ/Задача 2. Макрос SUB ДЗ.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#define SUB(a, b) (a) - (b)
+#define SUB1(a, b, c) ((a) - (b)) * (c)
+#define SUB2(a, b, c) ((a) - ((b) + (c))) * (c)
+
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    int a = 6;
+    int b = 5;
+    int c = 2;
+    std::cout << SUB(a, b) << std::endl;
+    std::cout << SUB1(a, b, c) << std::endl;
+    std::cout << SUB2(a, b, c) << std::endl;
+
+    return 0;
+}
